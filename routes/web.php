c3f572f036f7695112385bf32bca28b9a5e05031
lukast2410 <lt24k24@gmail.com>
2022-01-31 09:49:34 +0700

    Try Remove Google Controller

--- routes/web.php
+++ routes/web.php
@@ -20,47 +20,49 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Auth::routes(['verify' => true]);
+Auth::routes();
 
-Route::get('auth/google', [GoogleController::class, 'redirect'])->name('google.login');
-Route::get('auth/google/callback', [GoogleController::class, 'callback'])->name('google.callback');
+// TODO: Add some routes with the necessary middleware
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/profile/{nickname}', [UserController::class, 'show'])->name('user.profile');
-Route::get('/art/{id}', [ArtController::class, 'show'])->name('art.detail');
-Route::post('/email/resend-verification', [VerificationController::class, 'resendVerification'])->middleware(['auth', 'throttle:6,1'])->name('resend.verification');
+// Route::get('auth/google', [GoogleController::class, 'redirect'])->name('google.login');
+// Route::get('auth/google/callback', [GoogleController::class, 'callback'])->name('google.callback');
 
-// For User except admin
-Route::middleware(['auth', 'not.admin'])->group(function() {
-  Route::get('/bid/ongoing', [BidController::class, 'index'])->name('bid.ongoing');
-  Route::get('/bid/history', [BidController::class, 'history'])->name('bid.history');
-  Route::get('/transaction-history', [BidController::class, 'transaction_history'])->name('transaction.history');
-  Route::post('/submit-bid', [BidController::class, 'store'])->name('submit.bid');
-  Route::delete('/cancel-bid/{id}', [BidController::class, 'destroy'])->name('cancel.bid');
-});
+// Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/profile/{nickname}', [UserController::class, 'show'])->name('user.profile');
+// Route::get('/art/{id}', [ArtController::class, 'show'])->name('art.detail');
+// Route::post('/email/resend-verification', [VerificationController::class, 'resendVerification'])->middleware(['auth', 'throttle:6,1'])->name('resend.verification');
 
-// Only for creator
-Route::middleware(['auth', 'verified', 'creator'])->group(function() {
-  Route::get('/create-art', [ArtController::class, 'create'])->name('create.art');
-  Route::post('/accept-offer', [ArtController::class, 'accept_offer'])->name('accept.offer');
-  Route::post('/create-art', [ArtController::class, 'store'])->name('create.art');
-  Route::get('/sell-history', [BidController::class, 'sell_history'])->name('sell.history');
-});
+// // For User except admin
+// Route::middleware(['auth', 'not.admin'])->group(function() {
+//   Route::get('/bid/ongoing', [BidController::class, 'index'])->name('bid.ongoing');
+//   Route::get('/bid/history', [BidController::class, 'history'])->name('bid.history');
+//   Route::get('/transaction-history', [BidController::class, 'transaction_history'])->name('transaction.history');
+//   Route::post('/submit-bid', [BidController::class, 'store'])->name('submit.bid');
+//   Route::delete('/cancel-bid/{id}', [BidController::class, 'destroy'])->name('cancel.bid');
+// });
 
-// User Only
-Route::middleware(['auth', 'verified', 'user.only'])->group(function() {
-  Route::get('/request-creator', [RequestCreatorController::class, 'create'])->name('request.creator');
-  Route::post('/request', [RequestCreatorController::class, 'store'])->name('request');
-});
+// // Only for creator
+// Route::middleware(['auth', 'verified', 'creator'])->group(function() {
+//   Route::get('/create-art', [ArtController::class, 'create'])->name('create.art');
+//   Route::post('/accept-offer', [ArtController::class, 'accept_offer'])->name('accept.offer');
+//   Route::post('/create-art', [ArtController::class, 'store'])->name('create.art');
+//   Route::get('/sell-history', [BidController::class, 'sell_history'])->name('sell.history');
+// });
 
-// Admin Only
-Route::middleware(['auth', 'admin'])->group(function() {
-  Route::get('/revenue', [BidController::class, 'revenue'])->name('revenue');
-  Route::put('/reject-request/{id}', [RequestCreatorController::class, 'reject'])->name('reject.request');
-  Route::put('/accept-request/{id}', [RequestCreatorController::class, 'accept'])->name('accept.request');
-});
+// // User Only
+// Route::middleware(['auth', 'verified', 'user.only'])->group(function() {
+//   Route::get('/request-creator', [RequestCreatorController::class, 'create'])->name('request.creator');
+//   Route::post('/request', [RequestCreatorController::class, 'store'])->name('request');
+// });
 
-// User except Creator
-Route::middleware(['auth', 'verified', 'not.creator'])->group(function() {
-  Route::get('/view-requests', [RequestCreatorController::class, 'index'])->name('view.requests');
-});
+// // Admin Only
+// Route::middleware(['auth', 'admin'])->group(function() {
+//   Route::get('/revenue', [BidController::class, 'revenue'])->name('revenue');
+//   Route::put('/reject-request/{id}', [RequestCreatorController::class, 'reject'])->name('reject.request');
+//   Route::put('/accept-request/{id}', [RequestCreatorController::class, 'accept'])->name('accept.request');
+// });
+
+// // User except Creator
+// Route::middleware(['auth', 'verified', 'not.creator'])->group(function() {
+//   Route::get('/view-requests', [RequestCreatorController::class, 'index'])->name('view.requests');
+// });
