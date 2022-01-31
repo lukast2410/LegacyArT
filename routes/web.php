7b5a32184618a01fb48783f697f968610218e11c
lukast2410 <lt24k24@gmail.com>
2022-01-31 18:11:02 +0700

    Remove Import Controllers, Remove Middleware in HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
-use App\Http\Controllers\ArtController;
-use App\Http\Controllers\Auth\GoogleController;
-use App\Http\Controllers\Auth\VerificationController;
-use App\Http\Controllers\BidController;
-use App\Http\Controllers\RequestCreatorController;
-use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
